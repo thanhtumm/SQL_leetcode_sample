010e4068e6ce4f8448014dc1db88970da0159017
thanhtumm <112602805+thanhtumm@users.noreply.github.com>
2023-05-19 21:44:04 +0700

    Create 511. Game Play Analysis I.sql

---
+++ 511. Game Play Analysis I.sql
@@ -0,0 +1,28 @@
+Game Play Analysis I - leetcode
+link : https://leetcode.com/problems/game-play-analysis-i/description/
+
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int)
+Truncate table Activity
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5')
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6')
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5')
+
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+
+# Write your MySQL query statement below
+select  player_id,min(event_date) as first_login
+from Activity
+group by player_id
