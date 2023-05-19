af54a1bd80e2aa4cb8f178127dc68707ff296fb3
thanhtumm <112602805+thanhtumm@users.noreply.github.com>
2023-05-19 21:46:12 +0700

    Update Rising Temperature.sql

--- Rising Temperature.sql
+++ Rising Temperature.sql
@@ -8,6 +8,21 @@ insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25
 insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+# Write your MySQL query statement below
 select distinct t2.id
 from Weather t1,Weather t2
 where datediff(t2.recordDate , t1.recordDate)=1 and t2.temperature > t1.temperature
